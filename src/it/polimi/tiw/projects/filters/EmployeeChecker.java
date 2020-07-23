0a2fe16cc234146b928752210edee68be4b0be8a
federicoromeo <10566536@polimi.it>
2020-07-23 19:18:08 +0200

    filters

---
+++ src/it/polimi/tiw/projects/filters/EmployeeChecker.java
@@ -0,0 +1,45 @@
+package it.polimi.tiw.projects.filters;
+
+import it.polimi.tiw.projects.beans.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class EmployeeChecker implements Filter {
+
+    public EmployeeChecker() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        System.out.println("Executing Employee filter...");
+        HttpServletRequest servletRequest = (HttpServletRequest) request;
+        HttpServletResponse servletResponse = (HttpServletResponse) response;
+
+        String loginPath = request.getServletContext().getContextPath() + "/index.html";
+
+        HttpSession session = ((HttpServletRequest) request).getSession();
+        User user;
+        user = (User) session.getAttribute("user");
+
+        if (!user.getRole().equals("employee")){
+        	servletResponse.sendRedirect(loginPath);
+            return;
+        }
+        // ok, go on
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
